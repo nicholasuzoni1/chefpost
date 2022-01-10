ebedca17f0b779e471cfd20e92d4f4bca4b49cee
Ammar Habib <ammar@techmatetech.com>
2022-01-10 13:48:39 +0500

    add pinterest meta tag.

--- wp-content/themes/ChefPost/header.php
+++ wp-content/themes/ChefPost/header.php
@@ -3,11 +3,11 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <meta name="description
-        "
+    <meta name="description"
           content="Book a personal chef for weekday meals, meals preps or events anytime. Available in South Florida. Book, Relax and Enjoy the Chefpost Experience">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
+    <meta name="p:domain_verify" content="13ab91ff2a46bad48366c3895d96ca44"/>
     <link rel="icon" type="image/png" sizes="16x16"
           href="<?php echo get_template_directory_uri() . '/assets/images/ic_cart.png' ?>">
     <title>Chefpost</title>
