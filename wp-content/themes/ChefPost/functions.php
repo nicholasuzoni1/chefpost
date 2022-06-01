565ddc6cfad53e11b1bbe0fd61cb6a6f230c19dc
Shehryar Nadeem Iqbal <shehryar.nadeem.qbal2@gmail.com>
2022-06-01 20:56:12 +0500

    Small fix

--- wp-content/themes/ChefPost/functions.php
+++ wp-content/themes/ChefPost/functions.php
@@ -583,9 +583,3 @@ function widgets_init()
 }
 
 add_action('widgets_init', 'widgets_init');
-
-function webp_upload_mimes( $existing_mimes ) {
-	$existing_mimes['webp'] = 'image/webp';
-	return $existing_mimes;
-}
-add_filter( 'mime_types', 'webp_upload_mimes' );
